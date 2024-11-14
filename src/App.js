55defa472cc455b68725ce33c2fb9d0b759959c3
PrajhanAlagu <prajhanalagu2006@gmail.com>
2024-11-14 21:22:07 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import LoginSignup from './project/pharmacy management system';
+import Homewebsite from './project/homepage';
+import OrderManagement from './project/order';
+import InventoryManagement from './project/inventoryman';
+import CustomerManagement from './project/customer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LoginSignup/>}/>
+      <Route path='/par' element={<Homewebsite/>}/>
+      <Route path="/inventory" element={<InventoryManagement />} />
+        <Route path="/order" element={<OrderManagement />} />
+        <Route path="/customer" element={<CustomerManagement />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
